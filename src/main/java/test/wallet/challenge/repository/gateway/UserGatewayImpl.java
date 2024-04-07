d0a4b110a7e76d703df54b70f79e34b733e52543
jose manuel burbano <jose.burbano@getpulpo.com>
2024-04-07 00:58:32 -0500

    wallet

--- src/main/java/test/wallet/challenge/repository/gateway/UserGatewayImpl.java
+++ src/main/java/test/wallet/challenge/repository/gateway/UserGatewayImpl.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 import test.wallet.challenge.domain.gateways.AccountRepository;
@@ -16,18 +15,12 @@ import test.wallet.challenge.repository.entities.User;
 import test.wallet.common.enums.ErrorCode;
 import test.wallet.common.exception.InfrastructureException;
 
-import java.util.Locale;
-
 @Repository
 @AllArgsConstructor
 public class UserGatewayImpl implements UserRepository {
 
     private static final Logger log = LoggerFactory.getLogger(UserGatewayImpl.class);
     private final ModelMapper modelMapper;
-
-    private final MessageSource messageSource;
-
-    private static final String RESOURCE_NAME = "UserGateway";
 
     private final SqlServerUserRepository userRepository;
 
@@ -39,10 +32,7 @@ public class UserGatewayImpl implements UserRepository {
         return Mono.defer(() -> {
             User existingUser = userRepository.findByUserId(userDTO.getUserId());
             if (existingUser != null) {
-                return Mono.error(new InfrastructureException(
-                        messageSource.getMessage("comun.recurso.existe",
-                                new Object[]{RESOURCE_NAME}, Locale.getDefault()),
-                        ErrorCode.FOUND));
+                return Mono.error(new InfrastructureException(ErrorCode.FOUND.getMessage(), ErrorCode.FOUND));
             } else {
                 return Mono.just(userDTO)
                         .map(dto -> modelMapper.map(dto, User.class))
