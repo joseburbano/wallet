1a410958f0e514fb792c0a79c0d09a33623ed4fe
jose manuel burbano <jose.burbano@getpulpo.com>
2024-04-07 13:49:12 -0500

    wallet

--- src/main/java/test/wallet/challenge/repository/entities/User.java
+++ src/main/java/test/wallet/challenge/repository/entities/User.java
@@ -18,8 +18,8 @@ import javax.validation.constraints.NotNull;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_sequence")
-    @SequenceGenerator(name = "users_sequence", sequenceName = "users_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence_users")
+    @SequenceGenerator(name = "sequence_users", sequenceName = "sequence_users", allocationSize = 1)
     private Integer id;
 
     @NotNull
